e29a93f774e231368aa9598f0e822b90d518ab69
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2022-02-16 11:50:29 -0800

    Initial Commit

---
+++ ApiPosts/ApiPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiPostsApp.swift
+//  ApiPosts
+//
+//  Created by Ierchenko Anna  on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
